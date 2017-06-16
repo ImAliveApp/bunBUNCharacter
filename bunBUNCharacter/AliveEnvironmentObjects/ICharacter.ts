ab617b72466e0d0d534768d930ca93113c630c36
hay12396 <hay12396@gmail.com>
2017-06-16 19:10:57 +0300

    .

--- bunBUNCharacter/AliveEnvironmentObjects/ICharacter.ts
+++ bunBUNCharacter/AliveEnvironmentObjects/ICharacter.ts
@@ -1,7 +1,10 @@
 ﻿interface ICharacter {
     Name: string;
     Id: string;
+
+    //use AgentConstants.X_CATEGORY to compare with the category that you need.
     Category: string;
+
     DownloadsCount: number;
     VotesCount: number;
     IsRecommended: boolean;
